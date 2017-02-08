b5e6469e87ac530a57c7ecebfbf2d3f27acf0d4f
Thibault Schueller <thibaults@eugensystems.com>
2017-02-08 10:34:00 +0100

    install: added whiptail dialogs for i3 and vim plugins post-install scripts

--- install.sh
+++ install.sh
@@ -3,36 +3,48 @@
 # The script assumes this path
 REPO=~/dotfiles/
 
+echo 'Installing from' $REPO
 ln -s {$REPO,~/}.aliases
 
-# Setup dicolors
 ln -s {$REPO,~/}.dircolors
-
-# Zsh setup
-ln -s {$REPO,~/}.zshrc
-wget -O ~/.zshrc_grml http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
-
 ln -s {$REPO,~/}.profile
 
 ln -s {$REPO,~/}.gitconfig
 ln -s {$REPO,~/}.gdbinit
 
-# Vim setup
+# htop Setup
+mkdir -p ~/.config/htop
+ln -s {$REPO,~/}.config/htop/htoprc
+
+# Zsh setup
+echo 'Zsh setup...'
+ln -s {$REPO,~/}.zshrc
+wget -O ~/.zshrc_grml http://git.grml.org/f/grml-etc-core/etc/zsh/zshrc
+
+echo 'Vim setup...'
 ln -s {$REPO,~/}.vimrc
 ln -s {$REPO,~/}.vim
 mkdir -p ~/.vim/{backup,swap,undo}
 vim +PluginInstall +qall
 
-# Install powerline patched fonts
-~/.vim/bundle/powerline-fonts/install.sh
+if whiptail --yesno "Vim plugins post-install?" 8 30
+then
+    echo 'Post-install...'
+    # Install powerline patched fonts
+    ~/.vim/bundle/powerline-fonts/install.sh
+    # Setup YCM
+    # requires some libs first, check the help (:h Ycm)
+    ~/.vim/bundle/YouCompleteMe/install.py --clang-completer
+fi
 
 # i3 Setup
-mkdir -p ~/.config/i3
-ln -s {$REPO,~/}.config/i3/config
-ln -s {$REPO,~/}.Xresources.d
-ln -s {$REPO,~/}.Xresources
+if whiptail --yesno "Install i3?" 8 24
+then
+    echo 'i3 setup...'
+    mkdir -p ~/.config/i3
+    ln -s {$REPO,~/}.config/i3/config
+    ln -s {$REPO,~/}.Xresources.d
+    ln -s {$REPO,~/}.Xresources
+fi
 
-# htop Setup
-mkdir -p ~/.config/htop
-ln -s {$REPO,~/}.config/htop/htoprc
-
+echo 'done'
